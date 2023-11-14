810b639ca4386631fe208db8955fb5fc6167212e
Santiago Luis Ocón <ocon.santiago@gmail.com>
2023-11-14 13:33:38 -0300

    Add success field to HTTP response

--- src/helpers/http-status.js
+++ src/helpers/http-status.js
@@ -13,6 +13,7 @@ export const HTTP_STATUS_CODE = {
 export const HTTP_RESPONSE = {
   OK: (response, data) => {
     return response.status(HTTP_STATUS_CODE.OK).json({
+      success: true,
       status: HTTP_STATUS_CODE.OK,
       statusMessage: 'SUCCESS',
       data
@@ -20,6 +21,7 @@ export const HTTP_RESPONSE = {
   },
   CREATED: (response, data) => {
     return response.status(HTTP_STATUS_CODE.CREATED).json({
+      success: true,
       status: HTTP_STATUS_CODE.CREATED,
       statusMessage: 'CREATED',
       data
@@ -27,6 +29,7 @@ export const HTTP_RESPONSE = {
   },
   BAD_REQUEST: (response, message, errorData) => {
     return response.status(HTTP_STATUS_CODE.BAD_REQUEST).json({
+      success: false,
       status: HTTP_STATUS_CODE.BAD_REQUEST,
       statusMessage: 'BAD REQUEST',
       message,
@@ -35,6 +38,7 @@ export const HTTP_RESPONSE = {
   },
   UNAUTHORIZED: (response, message) => {
     return response.status(HTTP_STATUS_CODE.UNAUTHORIZED).json({
+      success: false,
       status: HTTP_STATUS_CODE.UNAUTHORIZED,
       statusMessage: 'UNAUTHORIZED',
       message
@@ -42,6 +46,7 @@ export const HTTP_RESPONSE = {
   },
   FORBIDDEN: (response, message) => {
     return response.status(HTTP_STATUS_CODE.FORBIDDEN).json({
+      success: false,
       status: HTTP_STATUS_CODE.FORBIDDEN,
       statusMessage: 'FORBIDDEN',
       message
@@ -49,6 +54,7 @@ export const HTTP_RESPONSE = {
   },
   NOT_FOUND: (response, message) => {
     return response.status(HTTP_STATUS_CODE.NOT_FOUND).json({
+      success: false,
       status: HTTP_STATUS_CODE.NOT_FOUND,
       statusMessage: 'NOT FOUND',
       message
@@ -56,6 +62,7 @@ export const HTTP_RESPONSE = {
   },
   UNPROCESSABLE_ENTITY: (response, message, errorData) => {
     return response.status(HTTP_STATUS_CODE.UNPROCESSABLE_ENTITY).json({
+      success: false,
       status: HTTP_STATUS_CODE.UNPROCESSABLE_ENTITY,
       statusMessage: 'UNPROCESSABLE ENTITY',
       message,
@@ -64,6 +71,7 @@ export const HTTP_RESPONSE = {
   },
   INTERNAL_SERVER_ERROR: (response, message, error) => {
     return response.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json({
+      success: false,
       status: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
       statusMessage: 'INTERNAL SERVER ERROR',
       message,
